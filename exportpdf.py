aab68f7f5ba45f04c9642809567b8d1e40eb59f5
Walter Bender <walter@sugarlabs.org>
2014-07-15 11:06:00 -0400

    pagination code to support multiple pages of text

--- exportpdf.py
+++ exportpdf.py
@@ -38,12 +38,12 @@ def save_pdf(activity, tmp_file, nick, description=None):
     body = 12
 
     if activity._game.get_mode() == 'array':
-        PAGE_WIDTH = 545
-        PAGE_HEIGHT = 700
+        page_width = 545
+        page_height = 700
     else:
-        PAGE_WIDTH = 700
-        PAGE_HEIGHT = 545
-    pdf_surface = cairo.PDFSurface(tmp_file, PAGE_WIDTH, PAGE_HEIGHT)
+        page_width = 700
+        page_height = 545
+    pdf_surface = cairo.PDFSurface(tmp_file, page_width, page_height)
 
     fd = Pango.FontDescription('Sans')
     cr = cairo.Context(pdf_surface)
@@ -55,12 +55,12 @@ def save_pdf(activity, tmp_file, nick, description=None):
         y = TOP_MARGIN
 
     show_text(cr, fd, activity.metadata['title'], head, LEFT_MARGIN, y,
-              PAGE_WIDTH)
+              page_width, page_height)
     y += 4 * head
-    show_text(cr, fd, nick, body, LEFT_MARGIN, y, PAGE_WIDTH)
+    show_text(cr, fd, nick, body, LEFT_MARGIN, y, page_width, page_height)
     y += head
     show_text(cr, fd, time.strftime('%x', time.localtime()), body,
-              LEFT_MARGIN, y, PAGE_WIDTH)
+              LEFT_MARGIN, y, page_width, page_height)
 
     if activity._game.get_mode() == 'array':
         y += TOP_MARGIN * 3
@@ -68,14 +68,15 @@ def save_pdf(activity, tmp_file, nick, description=None):
         y += TOP_MARGIN * 2
 
     if description is not None:
-        show_text(cr, fd, description, body, LEFT_MARGIN, y, PAGE_WIDTH)
+        show_text(cr, fd, description, body, LEFT_MARGIN, y, page_width,
+                  page_height)
     cr.show_page()
 
     if activity._game.get_mode() == 'array':
         text = ''
         if 'text' in activity.metadata:
             text = activity.metadata['text']
-        one_page(activity, cr, fd, body, text, PAGE_WIDTH)
+        one_page(activity, cr, fd, body, text, page_width, page_height)
     else:
         save_page = activity._game.current_image
         for i in range(9):
@@ -83,11 +84,11 @@ def save_pdf(activity, tmp_file, nick, description=None):
             text = ''
             if 'text-%d' % i in activity.metadata:
                 text = activity.metadata['text-%d' % i]
-            page(activity, cr, fd, body, text, PAGE_WIDTH)
+            page(activity, cr, fd, body, text, page_width, page_height)
         activity._game.current_image = save_page
 
 
-def one_page(activity, cr, fd, body, text, page_width):
+def one_page(activity, cr, fd, body, text, page_width, page_height):
     w = h = int((4 * activity._game._space + 3 * activity._game._dot_size))
     xo = int((page_width - (w / 2)) * 0.5)
     yo = TOP_MARGIN
@@ -106,12 +107,13 @@ def one_page(activity, cr, fd, body, text, page_width):
         cr.restore()
     cr.restore()
 
-    show_text(cr, fd, text, body, LEFT_MARGIN, 350 + TOP_MARGIN, page_width)
+    show_text(cr, fd, text, body, LEFT_MARGIN, 350 + TOP_MARGIN, page_width,
+              page_height)
 
     cr.show_page()
 
 
-def page(activity, cr, fd, body, text, page_width):
+def page(activity, cr, fd, body, text, page_width, page_height):
     w = h = int((4 * activity._game._space + 3 * activity._game._dot_size))
     x = int((page_width - (w / 2)) * 0.67) + LEFT_MARGIN
     y = TOP_MARGIN
@@ -124,22 +126,57 @@ def page(activity, cr, fd, body, text, page_width):
     cr.fill()
     cr.restore()
 
-    show_text(cr, fd, text, body, LEFT_MARGIN, 350 + TOP_MARGIN, page_width)
+    show_text(cr, fd, text, body, LEFT_MARGIN, 350 + TOP_MARGIN, page_width,
+              page_height)
 
     cr.show_page()
 
 
-def show_text(cr, fd, label, size, x, y, page_width):
-    pl = PangoCairo.create_layout(cr)
+def show_text(cr, fd, label, size, x, y, page_width, page_height):
     fd.set_size(int(size * Pango.SCALE))
-    pl.set_font_description(fd)
+
+    # TODO: RTL support
+
     if type(label) == str or type(label) == unicode:
-        pl.set_text(label.replace('\0', ' '), -1)
+        text = label.replace('\0', ' ')
     else:
-        pl.set_text(str(label), -1)
-    pl.set_width((page_width - LEFT_MARGIN * 2) * Pango.SCALE)
-    cr.save()
-    cr.translate(x, y)
-    PangoCairo.update_layout(cr, pl)
-    PangoCairo.show_layout(cr, pl)
-    cr.restore()
+        text = str(label)
+
+    top = y
+    left = x
+    right = page_width - LEFT_MARGIN
+    bottom = page_height - TOP_MARGIN * 2
+
+    sentences = text.split('\n')
+    for sentence in sentences:
+        pl = PangoCairo.create_layout(cr)
+        pl.set_font_description(fd)
+        words = sentence.split(' ')
+        for word in words:
+            pl.set_text(word + ' ', -1)
+            width, height = pl.get_size()
+            width /= Pango.SCALE
+            height /= Pango.SCALE
+            if x + width > right:
+                x = LEFT_MARGIN
+                y += size * 1.5
+                if y > bottom:
+                    cr.show_page()
+                    pl = PangoCairo.create_layout(cr)
+                    pl.set_font_description(fd)
+                    y = TOP_MARGIN
+                    x = LEFT_MARGIN
+            cr.save()
+            cr.translate(x, y)
+            PangoCairo.update_layout(cr, pl)
+            PangoCairo.show_layout(cr, pl)
+            cr.restore()
+            x += width
+        x = LEFT_MARGIN
+        y += size * 1.5
+        if y > bottom:
+            cr.show_page()
+            pl = PangoCairo.create_layout(cr)
+            pl.set_font_description(fd)
+            y = TOP_MARGIN
+            x = LEFT_MARGIN
